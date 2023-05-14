8f9936d5b25f5900e10f676bababb32b7520129d
LShuXin <1569663570@qq.com>
2023-05-14 14:57:59 +0800

    feat: add history route

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,1 +1,17 @@
-export default () => <div>login page</div>;
+import { history } from 'umi';
+
+export default () => {
+  const goBack = function () {
+    history.goBack();
+  };
+
+  return (
+    <>
+      <div>
+        login page
+        <div onClick={goBack}>go back</div>
+      </div>
+      ;
+    </>
+  );
+};
--- src/pages/work1.tsx
+++ src/pages/work1.tsx
@@ -1,3 +1,24 @@
+import { history } from 'umi';
+
 export default () => {
-  return <div>work1 page</div>;
+  const toUserCenter = () => {
+    history.push('/user');
+  };
+
+  const toLogin = () => {
+    history.push({
+      pathname: '/login',
+      query: {
+        a: 'b',
+      },
+    });
+  };
+
+  return (
+    <>
+      <div>work1 page</div>
+      <div onClick={toUserCenter}>to user Center</div>
+      <div onClick={toLogin}>to login</div>
+    </>
+  );
 };
