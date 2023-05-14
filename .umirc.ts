16f7934f95df95682548d7d2a8adb2b357776f41
LShuXin <1569663570@qq.com>
2023-05-14 14:49:23 +0800

    feat: add title

--- .umirc.ts
+++ .umirc.ts
@@ -6,18 +6,19 @@ export default defineConfig({
   },
   // layout: {},
   routes: [
-    { path: '/user', component: '@/pages/user' },
-    { path: '/login', component: '@/pages/login' },
+    { path: '/user', component: '@/pages/user', title: '用户中心' },
+    { path: '/login', component: '@/pages/login', title: '登录' },
     {
       path: '/',
       component: '@/layouts/index',
       routes: [
         { path: '/index', redirect: '/work1' },
-        { path: '/work1', component: '@/pages/work1' },
+        { path: '/work1', component: '@/pages/work1', title: 'work1' },
         {
           path: '/work2',
           component: '@/pages/work2',
           wrappers: ['@/wrappers/auth'],
+          title: 'work2',
         },
       ],
     },
