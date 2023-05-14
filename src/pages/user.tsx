f2ee745eb7a64aff2d43c08d23b2ff004f98f769
LShuXin <1569663570@qq.com>
2023-05-14 15:01:56 +0800

    feat: add Link tag

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'umi';
+
 export default function User() {
   return (
     <div>
       <p>这是用户页面</p>
+      <Link to="/work1">a link tag navagate to work1</Link>
     </div>
   );
 }
